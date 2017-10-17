3fb8510589baa2af8f1379248e9cd6b14da8b29b
Nikola Kuridza <kuridzan@gmail.com>
2017-10-18 01:36:26 +0200

    tests simplified

--- tests.php
+++ tests.php
@@ -1,71 +1,59 @@
 <?php
 require __DIR__ . '/app.php';
 
+$uri = 'http://localhost:8000';
+$client = new \GuzzleHttp\Client(['base_uri' => $uri, 'http_errors' => false]);
+
 $passed = 0;
 $failed = 0;
 $output = '';
-function testOutput($input, $expected, $message, $request, $body) {
-    global $passed, $failed, $output;
+$body = '';
+//function testOutput($input, $expected, $message, $request, $body) {
+function testOutput($message, $method, $path, $data = [], $expected = 200) {
+    global $passed, $failed, $output, $client, $body;
+    $response = $client->{$method}($path, $data);
+    $input = $response->getStatusCode();
+    $body = $response->getBody();
+    $body = json_decode($body, true);
     $p = preg_match('/' . $input . '/', $expected);
     if($p) $passed++; else $failed++;
     if (php_sapi_name() === 'cli') {
         $output .= $message . PHP_EOL;
-        $output .= $request . PHP_EOL;
-        $output .= 'Response: ' . json_encode(json_decode($body), JSON_PRETTY_PRINT) . PHP_EOL;
+        $output .= $method . ' ' . $path . PHP_EOL;
+        $output .= 'Response: ' . json_encode($body, JSON_PRETTY_PRINT) . PHP_EOL;
         $output .= 'Input / Expected' . PHP_EOL . $input . ' / ' . $expected . PHP_EOL;
         $output .= $p ? 'Test passed.' : 'Test failed';
         $output .= PHP_EOL . '____________________________________________' . PHP_EOL;
     } else {
         $output .= '<h1>' . $message . '</h1>';
-        $output .= '<h2>' . $request . '</h2>';
-        $output .= 'Response: <pre>' . json_encode(json_decode($body), JSON_PRETTY_PRINT) . '</pre>';
+        $output .= '<h2>' . $method . ' ' . $path . '</h2>';
+        $output .= 'Response: <pre>' . json_encode($body, JSON_PRETTY_PRINT) . '</pre>';
         $output .= 'Input / Expected <br>' . $input . ' / ' . $expected . '<br>';
         $output .= $p ? 'Test passed.' : 'Test failed';
         $output .= '<hr>';
     }
 }
 
-$uri = 'http://localhost:8000';
+testOutput('Ok request', 'GET', '/book');
+testOutput('Not found request', 'GET', '/book/0', [], 404);
+testOutput('Unauthorized request', 'POST', '/book', [], 403);
+testOutput('Bad request', 'POST', '/book', ['auth' => ['nikola', '1234']], 500);
 
-$client = new \GuzzleHttp\Client(['base_uri' => $uri, 'http_errors' => false]);
-
-$response = $client->get('/book');
-
-testOutput($response->getStatusCode(), 200, 'Ok request', 'GET /book', $response->getBody());
-
-$response = $client->get('/book/0');
-
-testOutput($response->getStatusCode(), 404, 'Not found request', 'GET /book/0', $response->getBody());
-
-$response = $client->post('/book');
-
-testOutput($response->getStatusCode(), 403, 'Unauthorized request', 'POST /book', $response->getBody());
-
-$response = $client->post('/book', ['auth' => ['nikola', '1234']]);
-
-testOutput($response->getStatusCode(), 500, 'Bad request', 'POST /book', $response->getBody());
-
-$response = $client->post('/book', [
+testOutput('Insert request', 'POST', '/book', [
     'auth' => ['nikola', '1234'],
     'json' => ['title' => 'Telefonoteka', 'author' => 'TFT', 'year' => 2016],
 ]);
 
-$id = \GuzzleHttp\json_decode($response->getBody(), true)['id'];
+$id = $body['id'];
 
-testOutput($response->getStatusCode(), 200, 'Insert request', 'POST /book', $response->getBody());
-
-$response = $client->patch('/book/' . $id, [
+testOutput('Update request', 'PATCH', '/book/' . $id, [
     'auth' => ['nikola', '1234'],
     'json' => ['title' => 'TeleFonoTeka', 'author' => 'TFT', 'year' => 2017],
 ]);
 
-testOutput($response->getStatusCode(), 200, 'Update request', 'PATCH /book', $response->getBody());
+testOutput('Delete request', 'DELETE', '/book/' . $id, ['auth' => ['nikola', '1234']], 204);
 
-$response = $client->delete('/book/' . $id, ['auth' => ['nikola', '1234']]);
-
-testOutput($response->getStatusCode(), 204, 'Delete request', 'DELETE /book', $response->getBody());
-
-$response = $client->post('/batch/book', [
+testOutput('Batch insert request', 'POST', '/batch/book', [
     'auth' => ['nikola', '1234'],
     'json' => [
         ['title' => 'Telefonoteka', 'author' => 'TFT', 'year' => 2016],
@@ -74,12 +62,9 @@ $response = $client->post('/batch/book', [
     ],
 ]);
 
-$json = \GuzzleHttp\json_decode($response->getBody(), true);
-$ids = array_column($json, 'id');
+$ids = array_column($body, 'id');
 
-testOutput($response->getStatusCode(), 200, 'Batch insert request', 'POST /batch-book', $response->getBody());
-
-$response = $client->patch('/batch/book', [
+testOutput('Batch update request', 'PATCH', '/batch/book', [
     'auth' => ['nikola', '1234'],
     'json' => [
         ['id' => $ids[0], 'title' => 'Telefonoteka1', 'author' => 'TFT', 'year' => 2016],
@@ -88,19 +73,14 @@ $response = $client->patch('/batch/book', [
     ],
 ]);
 
-testOutput($response->getStatusCode(), 200, 'Batch update request', 'PATCH /batch-book', $response->getBody());
-
-$response = $client->delete('/batch/book', [
+testOutput('Batch delete request', 'DELETE', '/batch/book', [
     'auth' => ['nikola', '1234'],
     'json' => [
         ['id' => $ids[0]],
         ['id' => $ids[1]],
         ['id' => $ids[2]],
     ],
-]);
-
-testOutput($response->getStatusCode(), 204, 'Batch delete request', 'DELETE /batch-book', $response->getBody());
-
+], 204);
 
 echo 'Passed test: ' . $passed . PHP_EOL;
 echo 'Failed test: ' . $failed . '<hr>';
